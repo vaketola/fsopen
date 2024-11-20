96a397e4431601250eecdf43c1e5530de9234dd4
vaketola <vili-arttu.ketola@helsinki.fi>
2024-11-20 20:21:43 +0200

    complete 2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -43,9 +43,12 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
     } else {
       const newPerson = {name:newName, number:newNumber}
-      setPersons(persons.concat(newPerson))
-      setNewName('')
-      setNewNumber('')
+      axios.post('http://localhost:3001/persons', newPerson)
+           .then(response => {
+            setPersons(persons.concat(response.data))
+            setNewName('')
+            setNewNumber('')
+           })
     }
   }
 
