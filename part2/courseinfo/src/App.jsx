94a8a65c4df6e6fbfb47add67784e7c36506d232
vaketola <vili-arttu.ketola@helsinki.fi>
2024-11-20 17:21:16 +0200

    complete 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,50 +1,4 @@
-const Header = (props) => {
-  return (
-    <>
-      <h2>
-        {props.course.name}
-      </h2>
-    </>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <>
-      <p>
-        {props.part.name} {props.part.exercises}
-      </p>
-    </>
-  )
-}
-
-const Content = ({ course }) => {
-
-  return (
-    <>
-      {course.parts.map((part, id) => <Part key={id} part={part} />)}
-    </>
-  )
-}
-
-const Total = ({ course }) => {
-  return (
-    <b>
-      total of {course.parts.reduce((total, part) => total + part.exercises, 0)} exercises
-    </b>
-  )
-}
-
-const Course = (props) => {
-
-  return (
-    <div>
-      <Header  course={props.course} />
-      <Content course={props.course} />
-      <Total   course={props.course} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
@@ -101,12 +55,6 @@ const App = () => {
     </>
   )
 }
-  // return (
-  //   <div>
-  //     <Header  course={course} />
-  //     <Content course={course} />
-  //     <Total   course={course} />
-  //   </div>
-  // )
+
 
 export default App
