3865e8facd3287d5344135df6a8333cb075f25cb
vaketola <vili-arttu.ketola@helsinki.fi>
2024-11-22 20:21:50 +0200

    complete 3.20

--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -19,7 +19,15 @@ const personSchema = new mongoose.Schema({
     number: {
         type: String,
         minLength: 8,
-        required: true
+        required: true,
+        validate: {
+            validator: function(v) {
+                // Some people, when confronted with a problem, think 
+                // "I know, I'll use regular expressions." 
+                // Now they have two problems.
+                return /^\d{2,3}-\d+$/.test(v)
+            }
+        }
     }
 })
 personSchema.set('toJSON', {
