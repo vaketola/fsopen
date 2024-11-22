f934f03648f46e3a59e7effcf698aa703c67e191
vaketola <vili-arttu.ketola@helsinki.fi>
2024-11-22 17:03:23 +0200

    complete 3.12

--- part3/phonebook_backend/mongo.js
+++ part3/phonebook_backend/mongo.js
@@ -1,59 +1,47 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
-    console.log('give password as argument')
+if (process.argv.length < 3 || process.argv.length > 5) {
+    console.log('command usage:')
+    console.log('print all: node mongo.js <password>')
+    console.log('add a new: node mongo.js <password> <name> <number>')
+    
     process.exit(1)
 } 
 
+const password = process.argv[2]
+const url = `mongodb+srv://vaketola-fsopen:${password}@fsopen.pzqao.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=fsopen`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name:   String,
+    number: String
+})
+const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
-    const password = process.argv[2]
-    const url = `mongodb+srv://vaketola-fsopen:${password}@fsopen.pzqao.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=fsopen`
-
-    mongoose.set('strictQuery', false)
-    mongoose.connect(url)
-
-    const personSchema = new mongoose.Schema({
-        name:   String,
-        number: String
-    })
-    const Person = mongoose.model('Person', personSchema)
-
     Person.find({}).then(result => {
+        console.log('phonebook:')
         result.forEach(person => {
-            console.log(person)
+            console.log(`${person.name} ${person.number}`)
         })
-        mongoose.connection.close()
-        process.exit(1)
-    })
-} else if (process.argv.length === 5) {
-    const password = process.argv[2]
-    const url = `mongodb+srv://vaketola-fsopen:${password}@fsopen.pzqao.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=fsopen`
-
-    const name   = process.argv[3]
-    const number = process.argv[4]
-
-    mongoose.set('strictQuery', false)
-    mongoose.connect(url)
-
-    const personSchema = new mongoose.Schema({
-        name:   String,
-        number: String
-    })
-    const Person = mongoose.model('Person', personSchema)
-
-    const person = new Person({
-        "name":   {name}, 
-        "number": {number}
-    })
-
-    person.save().then(result => {
-        console.log('note saved!')
         mongoose.connection.close()
         process.exit(1)
     })
 }
 
-console.log('incorrect command usage')
-process.exit(1)
+if (process.argv.length === 5) {
+    const name   = process.argv[3]
+        const number = process.argv[4]
+        const person = new Person({
+            "name":   name, 
+            "number": number
+        })
 
+        person.save().then(() => {
+            console.log(`added ${name} number ${number} to phonebook`)
+            mongoose.connection.close()
+            process.exit(1)
+        })
+}
