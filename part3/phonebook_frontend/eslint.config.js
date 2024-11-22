2e513b6f5ebc00e55e4a01e08c34caab714026e2
vaketola <vili-arttu.ketola@helsinki.fi>
2024-11-22 21:05:02 +0200

    complete 3.22

--- part3/phonebook_frontend/eslint.config.js
+++ part3/phonebook_frontend/eslint.config.js
@@ -1,38 +1,48 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from "globals";
+import stylisticJs from '@stylistic/eslint-plugin-js'
 
+
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ['dist'] },
+  //js.configs.recommended,
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.js"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
       },
+      ecmaVersion: "latest",
     },
-    settings: { react: { version: '18.3' } },
     plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      '@stylistic/js': stylisticJs
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      '@stylistic/js/indent': [
+        'error',
+        2
       ],
+      '@stylistic/js/linebreak-style': [
+        'error',
+        'unix'
+      ],
+      '@stylistic/js/quotes': [
+        'error',
+        'single'
+      ],
+      '@stylistic/js/semi': [
+        'error',
+        'never'
+      ],
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
+      'error', 'always'
+      ],
+      'arrow-spacing': [
+      'error', { 'before': true, 'after': true },
+      ],
+      'no-console': 'off',
     },
   },
 ]
