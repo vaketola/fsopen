7590fe6c593c71d30bd4bdbe662ab36184a661d8
vaketola <vili-arttu.ketola@helsinki.fi>
2024-11-22 18:52:12 +0200

    complete 3.16

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -7,21 +7,19 @@ const path = require('path')
 const app = express()
 
 
-// const randomId = (max) => Math.floor(Math.random()*max)
-
+// most middleware
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use((request, response, next) => {
     request.requestTime = new Date()
     next()
 })
-
 app.use(express.json())
+
 app.use(express.static('dist'))
 
 morgan.token('body', (request, response) => {return JSON.stringify(request.body)})
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-// This is equivalent to 'tiny' + body (from the docs)
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body')) // This is equivalent to 'tiny' + body (from the docs)
 
 app.use(cors({origin: 'http://localhost:5173'}))
 
@@ -38,19 +36,23 @@ app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {response.json(persons)})
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    }).catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id).then(() => {
         response.status(204).end()
-    })
+    }).catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     if (!body) {
         return response.status(400).json({error: 'invalid request content'})
@@ -73,12 +75,22 @@ app.post('/api/persons', (request, response) => {
 
         person.save().then(savedPerson =>{
             response.json(savedPerson)
-        })
-    }).catch(error => {
-        console.log(error)
-        response.status(500).json({error: 'failed to find person'})
-    })
+        }).catch(error => next(error))
+    }).catch(error => next(error))
 })
+
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    } 
+
+    next(error)
+}
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
