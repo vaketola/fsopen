e1834b5447bf583ea4c527909cd77d8f06e5e967
vaketola <vili-arttu.ketola@helsinki.fi>
2024-11-22 18:03:01 +0200

    remove cli logic from models for Render deployment

--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -1,14 +1,6 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3 || process.argv.length > 5) {
-    console.log('command usage:')
-    console.log('print all: node mongo.js <password>')
-    console.log('add a new: node mongo.js <password> <name> <number>')
-    
-    process.exit(1)
-} 
-
 const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery', false)
@@ -29,32 +21,7 @@ personSchema.set('toJSON', {
       delete returnedObject.__v
     }
 })
+
 const Person = mongoose.model('Person', personSchema)
-
-if (process.argv.length === 3) {
-    Person.find({}).then(result => {
-        console.log('phonebook:')
-        result.forEach(person => {
-            console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close()
-        process.exit(1)
-    })
-}
-
-if (process.argv.length === 5) {
-    const name   = process.argv[3]
-    const number = process.argv[4]
-    const person = new Person({
-        "name":   name, 
-        "number": number
-    })
-
-    person.save().then(() => {
-        console.log(`added ${name} number ${number} to phonebook`)
-        mongoose.connection.close()
-        process.exit(1)
-    })
-}
 
 module.exports = mongoose.model('Person', personSchema)
