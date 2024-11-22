407f4ce36c8d9b09792b60c0de3e229439a9fe8f
vaketola <vili-arttu.ketola@helsinki.fi>
2024-11-22 18:28:42 +0200

    update function to current version

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -45,7 +45,7 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndRemove(request.params.id).then(() => {
+    Person.findByIdAndDelete(request.params.id).then(() => {
         response.status(204).end()
     })
 })
