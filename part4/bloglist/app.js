38c2535b6cea80d30d28392f928587d92d86c904
vaketola <vili-arttu.ketola@helsinki.fi>
2024-11-25 18:11:56 +0200

    refactor into app.js

---
+++ part4/bloglist/app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+app.use(express.json()) 
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+mongoose.set('strictQuery', false)
+mongoose.connect(config.MONGODB_URI).then(result => {
+  logger.info('connected to MongoDB')
+}).catch(error => {
+  logger.error('error connecting to MongoDB', error.message)
+})
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+  .find({})
+  .then(blogs => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+  .save()
+  .then(result => {
+    response.status(201).json(result)
+  })
+})
+
+module.exports = app
