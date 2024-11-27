1a37cab80a1038e3ea78d0cba2c7d0e8d6ed4ec1
vaketola <vili-arttu.ketola@helsinki.fi>
2024-11-27 20:48:59 +0200

    complete 4.20

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -19,9 +19,10 @@ mongoose.connect(config.MONGODB_URI).then(result => {
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.errorHandler)
+app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
-app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -16,4 +16,16 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
-module.exports = { errorHandler }
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  } else {
+    request.token = null
+  }
+  next()
+}
+
+
+module.exports = { tokenExtractor, errorHandler }
