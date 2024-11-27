068c5d4076021fb74c6888b1c985c8c432e37268
vaketola <vili-arttu.ketola@helsinki.fi>
2024-11-27 15:51:49 +0200

    complete 4.10

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -5,11 +5,11 @@ blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
   response.json(blogs)
 })
-  
+
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
   const result = await blog.save()
   response.status(201).json(result)
 })
-  
+
 module.exports = blogsRouter
