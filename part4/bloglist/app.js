dc57859aafd54af20399eeb7ee29980401ea8f1d
vaketola <vili-arttu.ketola@helsinki.fi>
2024-11-27 20:26:39 +0200

    complete 4.18

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ const cors        = require('cors')
 const mongoose    = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const logger      = require('./utils/logger')
 const config      = require('./utils/config')
 
@@ -19,5 +20,6 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,8 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog        = require('../models/blog')
+const jwt         = require('jsonwebtoken')
 const User        = require('../models/user')
-const mongoose    = require('mongoose')
 require('express-async-errors')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user',{ username: 1, name: 1, id: 1 })
@@ -11,7 +19,11 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
-  const user = await User.findById(request.body.user.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   
   if (!blog.likes) blog.likes = 0
   if (!blog.title || !blog.url) {
