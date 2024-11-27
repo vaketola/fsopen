ecf147182ae4b1f11e15c5fe11ac23c9ed3ace99
vaketola <vili-arttu.ketola@helsinki.fi>
2024-11-27 16:50:21 +0200

    complete 4.13

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, describe, after, before } = require('node:test')
+const { test, describe, after, before, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -9,10 +9,15 @@ const api = supertest(app)
 let initLength = 0
 before(async () => {
   const Blog = require('../models/blog')
-  const blogs = await Blog.find({});
-  initLength = blogs.length;
+  const blogs = await Blog.find({})
+  initLength = blogs.length
 })
-
+let lastId = null
+beforeEach(async () => {
+  const Blog = require('../models/blog')
+  const blogs = await Blog.find({})
+  lastId = blogs.slice(-1)[0]._id
+})
 
 describe('HTTP GET request to /api/blogs', () => {
   test('blogs returned as json', async() => {
@@ -38,9 +43,9 @@ describe('HTTP GET request to /api/blogs', () => {
 describe('HTTP POST request to /api/blogs', () => {
   test('post succeeds with valid data', async() => {
     const newBlog = {
-      "author": `Test Author${initLength+2}`,
-      "title": `This is a test title${initLength+2}`,
-      "url": `https://fullstackopen.com/en/part${initLength+2}/`,
+      "author": `Test Author${initLength+1}`,
+      "title": `This is a test title${initLength+1}`,
+      "url": `https://fullstackopen.com/en/part${initLength+1}/`,
     }
 
     await api.post('/api/blogs')
@@ -56,19 +61,19 @@ describe('HTTP POST request to /api/blogs', () => {
 
   test('saved blog name is in database', async() => {
     const response = await api.get('/api/blogs')
-    const savedBlog = response.body.find((blog) => blog.title === `This is a test title${initLength+2}`)
+    const savedBlog = response.body.find((blog) => blog.title === `This is a test title${initLength+1}`)
     assert(savedBlog)
   })
 
   test('missing likes property defaulted to zero', async() => {
     const response = await api.get('/api/blogs')
-    const savedBlog = response.body.find((blog) => blog.title === `This is a test title${initLength+2}`)
+    const savedBlog = response.body.find((blog) => blog.title === `This is a test title${initLength+1}`)
     assert.strictEqual(savedBlog.likes, 0)
   })
 
   test('return 400 bad request if title missing', async() => {
     const newBlog = {
-      "author": `Test Author${initLength+3}`,
+      "author": `Test Author${initLength+2}`,
       "url": `https://fullstackopen.com/en/part${initLength+3}/`,
       "likes": 502 + initLength
     }
@@ -80,8 +85,8 @@ describe('HTTP POST request to /api/blogs', () => {
 
   test('return 400 bad request if url missing', async() => {
     const newBlog = {
-      "title": `This is a test title${initLength+4}`,
-      "author": `Test Author${initLength+4}`,
+      "title": `This is a test title${initLength+3}`,
+      "author": `Test Author${initLength+3}`,
       "likes": 503 + initLength
     }
 
@@ -91,6 +96,17 @@ describe('HTTP POST request to /api/blogs', () => {
   })
 })
 
+describe('HTTP DELETE request to /api/blogs', () => {
+  test('return 204 on delete request', async() => {
+    console.log(lastId)
+    await api.delete(`/api/blogs/${lastId}`).expect(204)
+  })
+
+  test('correct number of blogs', async() => {
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, initLength)
+  })
+})
 
 after(async () => {
   await mongoose.connection.close()
