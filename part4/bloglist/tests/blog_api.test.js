025584226df9ed6a4fd294efa5a27802e5cd202e
vaketola <vili-arttu.ketola@helsinki.fi>
2024-11-27 15:48:28 +0200

    complete test of 4.10

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,10 +1,18 @@
-const { test, describe, after } = require('node:test')
+const { test, describe, after, before } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
+
+let initLength = 0
+before(async () => {
+  const Blog = require('../models/blog')
+  const blogs = await Blog.find({});
+  initLength = blogs.length;
+})
+
 
 describe('HTTP GET request to /api/blogs', () => {
   test('blogs returned as json', async() => {
@@ -21,12 +29,38 @@ describe('HTTP GET request to /api/blogs', () => {
     })
   })
 
-  test('there are three blogs', async() => {
-    const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body.length, 3)
+  test('correct number of blogs', async() => {
+    const response = await api.get('/api/blogs')    
+    assert.strictEqual(response.body.length, initLength)
   })
 })
 
+describe('HTTP POST request to /api/blogs', () => {
+  test('post succeeds with valid data', async() => {
+    const newBlog = {
+      "author": `Test Author${initLength+1}`,
+      "title": `This is a test title${initLength+1}`,
+      "url": `https://fullstackopen.com/en/part${initLength+1}/`,
+      "likes": 501 + initLength
+    }
+
+    await api.post('/api/blogs')
+             .send(newBlog)
+             .expect(201)
+             .expect('Content-Type', /application\/json/)
+  })
+
+  test('correct number of blogs', async() => {
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, initLength+1)
+  })
+
+  test('saved blog name is in database', async() => {
+    const response = await api.get('/api/blogs')
+    const savedBlog = response.body.find((blog) => blog.title === `This is a test title${initLength+1}`)
+    assert(savedBlog)
+  })
+})
 
 
 after(async () => {
