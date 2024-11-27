40832bff35f2fe0299c5ee2aadae2d625ea7c0c5
vaketola <vili-arttu.ketola@helsinki.fi>
2024-11-27 20:33:37 +0200

    complete 4.19

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,7 @@ const mongoose    = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const middleware  = require('./utils/middleware')
 const logger      = require('./utils/logger')
 const config      = require('./utils/config')
 
@@ -21,5 +22,6 @@ app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use(middleware.errorHandler)
 
 module.exports = app
---
+++ part4/bloglist/utils/middleware.js
@@ -0,0 +1,19 @@
+
+const errorHandler = (error, request, response, next) => {
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  } 
+  if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
+    return response.status(400).json({ error: 'expected `username` to be unique' })
+  } 
+  if (error.name ===  'JsonWebTokenError') {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  next(error)
+}
+
+module.exports = { errorHandler }
