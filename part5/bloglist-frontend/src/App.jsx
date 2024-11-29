646a9e1efaa29cba1b99b5f6f2220851c6a87423
vaketola <vili-arttu.ketola@helsinki.fi>
2024-11-29 15:24:46 +0200

    complete 5.2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
     
     try {
       const user = await loginService.login({ username, password })
+      window.localStorage.setItem('loggedBlogAppUser', JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
@@ -28,27 +29,37 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   if (user === null) {
     return (
       <div>
         <h2>blogs</h2>
-        <div>
-          username <input 
-            type='text'
-            value={username}
-            name='username'
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password <input 
-            type='text'
-            value={password}
-            name='password'
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button onClick={handleLogin}>login</button>
+        <form onSubmit={handleLogin}>
+          <div>
+            username <input 
+              type='text'
+              value={username}
+              name='username'
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password <input 
+              type='text'
+              value={password}
+              name='password'
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button onClick={handleLogin}>login</button>
+        </form>
       </div>
     )
   }
@@ -59,6 +70,11 @@ const App = () => {
       <p>
         {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
       </p>
+      <button onClick={() => {
+        window.localStorage.removeItem('loggedBlogAppUser')
+        setUser(null)
+      }}>logout
+      </button>
     </div>
   )
 }
