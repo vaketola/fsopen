b3489ed9945adb00eac99f09b7e014481abc6e4d
vaketola <vili-arttu.ketola@helsinki.fi>
2024-12-03 23:28:30 +0200

    complete 6.20

---
+++ part6/query-anecdotes/README.md
@@ -0,0 +1,3 @@
+cloned from
+https://github.com/fullstack-hy2020/query-anecdotes
+to continue exercise 6 as instructed
---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,51 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+const App = () => {
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: () => axios.get('http://localhost:3001/anecdotes')
+      .then(response => response.data)
+  })
+  // console.log(JSON.parse(JSON.stringify(result)))
+
+  if (result.isLoading) return <div>loading anecdote service</div>
+  if (result.isError) {
+    return (
+      <div>
+        anecdote service not available due to problems in server
+      </div>
+    )
+  }
+
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+  const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+    
+      <Notification />
+      <AnecdoteForm />
+    
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
