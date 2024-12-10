5fa3eaa0d6a6b8f22e6aa84fd0ee2de84bfc6d70
vaketola <vili-arttu.ketola@helsinki.fi>
2024-12-10 05:03:23 +0200

    complete 9.26

--- part9/patientor-frontend/src/components/PatientInfoPage.tsx
+++ part9/patientor-frontend/src/components/PatientInfoPage.tsx
@@ -1,12 +1,15 @@
 import { useParams } from "react-router-dom";
 import {
-  Entry, Patient, Diagnosis
+  Entry, Patient, Diagnosis, HealthCheckRating,
+  OccupationalHealthcareEntry, HospitalEntry, HealthCheckEntry,
 } from "../types";
 import patientService from "../services/patients";
-import { Male, Female } from '@mui/icons-material';
+import {
+  Male, Female,
+  Favorite, Work, MedicalServices,
+} from '@mui/icons-material';
 import { useEffect, useState } from "react";
-
-const Other = (): JSX.Element => <></>;
+import { Box } from "@mui/material";
 
 const findDiagnosis = (code: string, diagnoses: Array<Diagnosis>): string => {
   const diagnosis: Diagnosis | undefined = diagnoses.find(diagnosis => diagnosis.code === code);
@@ -14,22 +17,126 @@ const findDiagnosis = (code: string, diagnoses: Array<Diagnosis>): string => {
   return '';
 };
 
-const PatientEntry = ({ entry, diagnoses }: {
-  entry: Entry,
-  diagnoses: Array<Diagnosis>
- }): JSX.Element => {
+const PatientOccupationalHealthcareEntry = ({ entry, diagnoses }: {
+    entry: OccupationalHealthcareEntry,
+    diagnoses: Array<Diagnosis>
+  }): JSX.Element => {
+  
+  if (entry.sickLeave) {
+    return (
+      <Box border={1} m={1}>
+        <Work fontSize='small' /> {entry.date} <b>{entry.employerName}</b>
+        <div><i>{entry.description}</i></div>
+        <ul>
+          {entry.diagnosisCodes?.map(code => 
+            <li key={code}>
+              {code} {findDiagnosis(code, diagnoses)}
+            </li>
+          )}
+        </ul>
+        <div>
+          sick leave {entry.sickLeave.startDate} - {entry.sickLeave.endDate}
+        </div>
+        <div>seen by {entry.specialist}</div>
+      </Box>
+    );
+  } else {
+    return (
+      <Box border={1} m={1}>
+        <Work fontSize='small' /> {entry.date} <b>{entry.employerName}</b>
+        <div><i>{entry.description}</i></div>
+        <ul>
+          {entry.diagnosisCodes?.map(code => 
+            <li key={code}>
+              {code} {findDiagnosis(code, diagnoses)}
+            </li>
+          )}
+        </ul>
+        <div>seen by {entry.specialist}</div>
+      </Box>
+    );
+  }
+};
+
+const PatientHospitalEntry = ({ entry, diagnoses }: {
+    entry: HospitalEntry,
+    diagnoses: Array<Diagnosis>
+  }): JSX.Element => {
+
   return (
-    <>
-      {entry.date} <i>{entry.description}</i>
+    <Box border={1} m={1}>
+      <MedicalServices fontSize='small' /> {entry.date}
+      <div><i>{entry.description}</i></div>
       <ul>
         {entry.diagnosisCodes?.map(code => 
           <li key={code}>
             {code} {findDiagnosis(code, diagnoses)}
-          </li>)
-        }
+          </li>
+        )}
       </ul>
-    </>
+      <div>discharge when {entry.discharge.criteria.toLowerCase()} {entry.discharge.date}</div>
+      <div>seen by {entry.specialist}</div>
+    </Box>
   );
+};
+
+const PatientHealthCheckEntry = ({ entry, diagnoses }: {
+    entry: HealthCheckEntry,
+    diagnoses: Array<Diagnosis>
+  }): JSX.Element => {
+
+  let heartColor = 'black';
+  switch (entry.healthCheckRating) {
+    case HealthCheckRating.Healthy:
+      heartColor = 'green';
+      break;
+    case HealthCheckRating.LowRisk:
+      heartColor = 'yellow';
+      break;
+    case HealthCheckRating.HighRisk:
+      heartColor = 'orange';
+      break;
+    case HealthCheckRating.CriticalRisk:
+      heartColor = 'red';
+      break;
+    default: 
+      heartColor = 'black';
+      break;
+  }
+  
+  return (
+    <Box border={1} m={1}>
+      <MedicalServices fontSize='small' /> {entry.date}
+      <div>
+        <Favorite sx={{ color: heartColor }} fontSize='small' />
+        <i>{entry.description}</i>
+      </div>
+      <ul>
+        {entry.diagnosisCodes?.map(code => 
+          <li key={code}>
+            {code} {findDiagnosis(code, diagnoses)}
+          </li>
+        )}
+      </ul>
+      <div>seen by {entry.specialist}</div>
+    </Box>
+  );
+};
+
+const PatientEntry = ({ entry, diagnoses }: {
+  entry: Entry,
+  diagnoses: Array<Diagnosis>
+ }): JSX.Element => {
+
+  switch (entry.type) {
+    case "OccupationalHealthcare":
+      return <PatientOccupationalHealthcareEntry entry={entry} diagnoses={diagnoses} />;
+    case "Hospital":
+      return <PatientHospitalEntry entry={entry} diagnoses={diagnoses} />;
+    case "HealthCheck":
+      return <PatientHealthCheckEntry entry={entry} diagnoses={diagnoses} />;
+    default: return <></>;
+  }
 };
 
 const PatientInfoPage = ({ diagnoses }: { diagnoses: Array<Diagnosis> }): JSX.Element => {
@@ -47,6 +154,8 @@ const PatientInfoPage = ({ diagnoses }: { diagnoses: Array<Diagnosis> }): JSX.El
 
     void fetchPatient();
   }, [id]);
+
+  const Other = (): JSX.Element => <></>;
 
   if (patient) {
     const Icon = 
